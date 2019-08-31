c1c29610a1d0b5300bf7bc1f86eea8a1e2b88dad
Ubuntu <ubuntu@ip-172-31-26-90.us-east-2.compute.internal>
2019-08-31 21:12:38 +0000

    Commit 2 With jenkins

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
    http.HandleFunc("/about me", func(w http.ResponseWriter, r *http.Request){
             fmt.Fprintf(w, "Hello,Stop knowing about and wasting time do something usefull ", html.EscapeString(r.URL.Path))
 })
+   http.HandleFunc("/about Us", func(w http.ResponseWriter, r *http.Request){
+            fmt.Fprintf(w, "Hello,Stop knowing about and wasting time do something useful and tech", html.EscapeString(r.URL.Path))
+})
 
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
