660b0ef84d99d19d208be23adbd576801f28fa2c
Raghuramch <42361356+Raghuramch@users.noreply.github.com>
2019-09-03 16:07:09 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,32 +2,20 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
 )
 
-func main() {
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-    
-   http.HandleFunc("/about me", func(w http.ResponseWriter, r *http.Request){
-            fmt.Fprintf(w, "Hello,Stop knowing about and wasting time do something usefull ", html.EscapeString(r.URL.Path))
-})
-   http.HandleFunc("/about me", func(w http.ResponseWriter, r *http.Request){
-            fmt.Fprintf(w, "Hello,Stop knowing about and wasting time do something" , html.EscapeString(r.URL.Path))
-})
-   http.HandleFunc("/about you", func(w http.ResponseWriter, r *http.Request){
-            fmt.Fprintf(w, "I dont know anything about you", html.EscapeString(r.URL.Path))
-})
-
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
-
+func handleRequests() {
+    http.HandleFunc("/", homePage)
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
+func main() {
+    handleRequests()
 }
