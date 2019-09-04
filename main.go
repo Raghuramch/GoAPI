80cc665e03f2f575f538283c4636673a0f7a7876
Ubuntu <ubuntu@ip-172-31-42-197.us-east-2.compute.internal>
2019-09-04 13:47:59 +0000

    wednesday commit

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"github.com/gorilla/mux"
+	"go-contacts/app"
+	"os"
+	"fmt"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
 func main() {
-    handleRequests()
+
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
 }
